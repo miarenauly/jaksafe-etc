5185e3db9a50bb54dcae5d42c9af2b0f1929dd0c
miarenauly <miarenauly@hotmail.com>
2016-06-28 13:43:56 +0700

    comment

--- script_cleaning_hazard.py
+++ script_cleaning_hazard.py
@@ -1,7 +1,3 @@
-import os, fnmatch
-from subprocess import call
-from osgeo import gdal
-call(["ls", "-l"])
 from PyQt4.QtCore import *
 import processing
 
@@ -13,24 +9,20 @@ RIVER_DKI = "E:/01 JakSAFE/09 PEMODELAN RW BANJIR QGIS/riverdki_polygon.shp"
 HAZARD_RIVER = "E:/01 JakSAFE/09 PEMODELAN RW BANJIR QGIS/TES/hazard_river.shp"
 HAZARD_FINAL = "E:/01 JakSAFE/09 PEMODELAN RW BANJIR QGIS/TES/hazard_final.shp"
 
-#Merging dengan SHP sungai
+#Union River shapefile with hazard
 processing.runalg("saga:union", HAZARD, RIVER_DKI, True, HAZARD_RIVER)
 
 
-#rename attribute
+#Rename attribute for River polygon
 open_sungai = iface.addVectorLayer(HAZARD_RIVER, "sungai", "ogr")
 srequest = QgsFeatureRequest().setFilterExpression(u'"KELAS" = 0')
 sids = [f.id() for f in open_sungai.getFeatures(srequest)]
 open_sungai.startEditing()
 for sfid in sids:
-    attrs = { 0 : 3 }
+    attrs = { 0 : 3 } #change polygon sungai jadi kelas 3
     open_sungai.dataProvider().changeAttributeValues({ sfid : attrs })
 open_sungai.commitChanges()
 
 
-"""JOINING ATTRIBUTE, masih, cek lagi"""
+#Union River, Hazard, and Boundar data
 processing.runalg("saga:union",HAZARD_RIVER, BOUNDARY_SHP,True,HAZARD_FINAL)
-""" This script supposed to work
-Catatan: hasil processing algorithm agak aneh, padahal kalo dicoba
-di luar sendiri hasilnya bener.
-Terus jangan lupa ambil nilai wilayah yang ada di sungai, bikin jadi kelas 3"""
